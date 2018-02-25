70c7d718ae71063f164df8a2a8121fd4bf8681e6
wenshao <wenshao314106@163.com>
2018-02-25 22:51:32 +0800

    重写路由

--- src/app.js
+++ src/app.js
@@ -12,81 +12,48 @@ const fetch = require('node-fetch');
 app.use(convert(bodyparser));
 app.use(convert(json()));
 app.proxy = true;
-const graphqlRouterPool = async (ctx, next) => {
-    console.log('===========111')
-    ctx.body = '1';
-    console.log(ctx.url);
-};
-const restfulRouterPool = (ctx, next) => {
-    ctx.body = '1';
-    console.log(ctx.url);
-};
+
 if (getServiceConfig().graphql) {
     const re = /^\/graphql\//;
-    app.use(async (ctx, next) => {
+    app.use(async(ctx, next) => {
         if (re.test(ctx.url)) {
-            const urlR =  ctx.url.replace(re, '');
-            if (!ZKInfo.graphql || !ZKInfo.graphql.nodes){  // graphql没有节点
+            const urlR = ctx.url.replace(re, '');
+            if (!graphqlZkData.isServiceExist(urlR)) {  // graphql没有节点
                 ctx.throw(500, 'not found in nodes');
-            } else if (!(urlR in ZKInfo.graphql.nodes)){    // 没有对应的服务
+            } else if (!graphqlZkData.isServiceNotNull(urlR)) {    // 没有对应的服务
                 ctx.throw(500, 'not found service');
             } else {
-                const parent = ZKInfo.graphql.nodes[urlR];
-                console.log(ctx.request.body,ctx.method);
-                if (ctx.method === 'GET') {
-                    ctx.body = await fetch('http://127.0.0.1:9000/graphql', {method: 'GET'}).then(res => res.json());
-                } else if (ctx.method === 'POST') {
-                    ctx.body =  await fetch('http://127.0.0.1:9000/graphql', {
+                if (ctx.method === 'POST') {
+                    const res = await fetch(`http://${graphqlZkData.getService(urlR)}/graphql`, {
                         method: 'POST',
-                        body:JSON.stringify(ctx.request.body),
-                        headers: { 'Content-Type': 'application/json' },
-                    }).then(res=>res.json());
+                        body: JSON.stringify(ctx.request.body),
+                        headers: {'Content-Type': 'application/json'},
+                        timeout: 2000
+                    });
+                    if (!res.ok) {
+                        ctx.throw(res.status, 'node post error');
+                    } else {
+                        ctx.body = await res.json();
+                    }
                 }
 
-                /*for (const id in parent.nodes) {
-
-                }*/
-
-                console.log(parent.data, parent.nodes);
             }
-            /*fetch('http://httpbin.org/post', {
-                method: 'POST',
-                body:    JSON.stringify(body),
-                headers: { 'Content-Type': 'application/json' },
-            })*/
+
         }
     });
 }
 if (getServiceConfig().restful) {
-    app.use(async (ctx, next) => {
+    app.use(async(ctx, next) => {
         if (ctx.url.test(/^\/restful/)) {
 
         }
     });
 }
 
-// 动态路由
-setListenerRouter({
-    onGraphqlInit: function (nodes) {
-        for (let id in nodes) {
-
-        }
-
-    },
-    onGraphqlAddService: function () {
-
-    },
-    onGraphqlAddNode: function () {
-
-    }
-
-});
 app.use(graphqlRouter.routes(), graphqlRouter.allowedMethods());
 app.use(restfulRouter.routes(), restfulRouter.allowedMethods());
 
 
-app.on('error', function (err, ctx) {
-    console.log(err);
-});
+
 
 module.exports = app;
