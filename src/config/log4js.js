08e51791f9f57f2281ed13c7a46b51d85d125804
wenshao <wenshao314106@163.com>
2018-04-22 18:23:31 +0800

    新版本

--- src/config/log4js.js
+++ src/config/log4js.js
@@ -2,10 +2,6 @@ const packageJson = require('../package.json');
 module.exports = {
     "appenders": {
         "console": {"type": "console"},
-        "kafka": {
-            "type": "kafka",
-            "socket_config": {"kafkaHost": process.env.KAFKA_URL, "topic": "global-log"}
-        }
     },
     "categories": {
         "default": {"appenders": ["console"], "level": "trace"}
--- src/config/service.js
+++ src/config/service.js
@@ -1,27 +1,18 @@
+const path = require('path');
 module.exports = {
+    "project_dir": path.resolve(__dirname, '..'),
     "node_env": NODE_ENV,
     "core_log": getLogger('core'),
+    "server_name": "gateway",
+    "service_id": "1.0.0.0",
     "zk": {
         "url": process.env.ZK_URL,
         "register": [
-            {
-                "path": `/${NODE_ENV}/gateway`,
-                "id": `${process.env.APP_IP}:${process.env.APP_PORT}`,
-                "data": {
-
-                }
-            }
         ]
     },
     "web": {
         "port": process.env.APP_PORT,
         "app": require('../app')
     },
-    "amq": {    // 可选
-        "mail": {
-            "topic": "basis-mail",
-            "host": process.env.KAFKA_URL,
-            "type": "kafka"
-        }
-    }
+
 };
